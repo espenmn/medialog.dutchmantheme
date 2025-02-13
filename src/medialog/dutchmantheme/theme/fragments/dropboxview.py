d4d81ce5144c65f55d04f3274b13b456caad1d24
Espen Moe-Nilssen <espen@medialog.no>
2025-02-13 19:04:17 +0100

    added waynes theme

--- src/medialog/dutchmantheme/theme/fragments/dropboxview.py
+++ src/medialog/dutchmantheme/theme/fragments/dropboxview.py
@@ -1,9 +1,37 @@
 def get_items(self):
-    linked = self.data['linked_folder'] or self.data['select_folder']
+    language = self.context.Language()
+    linked = self.data['linked_folder']
     if linked:
         folder = self.context.portal_catalog(UID=linked)
-        return folder[0].getObject()
-    return self.context
+        mappe =  folder[0].getObject()
+        folder_path = '/'.join(mappe.getPhysicalPath())
+        if mappe.portal_type != 'Collection':
+            # content_type = self.data['content_type']
+            # sort_on = self.data['sort_on']
+            # sort_order = str(self.data['sort_order'])
+            
+            return self.context.portal_catalog(path={'query': folder_path,}, Language=language  )
+
+def get_path(self):
+    linked = self.data['linked_folder']
+    folder = self.context.portal_catalog(UID=linked)
+    mappe =  folder[0].getObject()
+    return  '/'.join(mappe.getPhysicalPath())
+
+def list_items(self):
+    linked = self.data['linked_folder']
+    if linked:
+        folder = self.context.portal_catalog(UID=linked)
+        if folder[0].portal_type=='Collection':
+            return folder[0].getObject()
+
+
+def get_id(self):
+    if self.data['collectionfilter']:
+        return 'content-core'
+    else:
+        return 'fragment-{0}'.format(self.id)
+
 
 def get_margin(self):
     image_width = self.data['image_width']
