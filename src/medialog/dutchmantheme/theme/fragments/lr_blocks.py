ed161b9527077ad03ca17f0457b49380fb1d9ccb
Espen Moe-Nilssen <espen@medialog.no>
2025-02-18 10:40:14 +0100

    Working, hacked left_right block

--- src/medialog/dutchmantheme/theme/fragments/lr_blocks.py
+++ src/medialog/dutchmantheme/theme/fragments/lr_blocks.py
@@ -1,17 +1,35 @@
-def get_allitems(self):
-    linked = self.data['linked_folder']
-
+def get_items(self):
+    language = self.context.Language()
+    linked = self.data['linked_folder'] or self.data['select_folder'] or self.get_uid()
     if linked:
         folder = self.context.portal_catalog(UID=linked)
-        return folder[0].getObject()
+        mappe =  folder[0].getObject()
+        folder_path = '/'.join(mappe.getPhysicalPath())
+        if mappe.portal_type != 'Collection':
+            return self.context.portal_catalog(path={'query': folder_path, 'depth': 1}, Language=language)
+        
+        query = mappe.query
+        query_dict = {q['i']: q['v'] for q in query if 'i' in q and 'v' in q}
+        return self.context.portal_catalog(**query_dict) 
+ 
+def get_uid(self):
+    return self.context.UID()
 
-    selected = self.data['select_folder']
+def get_id(self):
+    if self.data['collectionfilter']:
+        return 'content-core'
+    else:
+        return 'fragment-{0}'.format(self.id)
 
-    if selected:
-        folder = self.context.portal_catalog(UID=selected)
-        return folder[0].getObject()
+def get_margin(self):
+    image_width = self.data['image_width']
+    return image_width /20
 
-    return None
+def get_width(self):
+    image_width = self.data['image_width']
+    max_images = self.data['max_images']
+    return image_width * 1.10 * max_images
+
 
 def editmode(self):
     form = self.request.form
