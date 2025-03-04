483f4dda3b629c404e4aab01cf83e2571ff2d132
Espen Moe-Nilssen <espen@medialog.no>
2025-03-04 20:47:22 +0100

    slider frament

--- src/medialog/dutchmantheme/theme/fragments/slider.py
+++ src/medialog/dutchmantheme/theme/fragments/slider.py
@@ -1,6 +1,11 @@
 def image_size(self):
     return self.data["image_size"]
 
+def get_items(self):
+    language = self.context.Language()
+    folder_path = self.data['folder_url']
+    return self.context.portal_catalog(path={'query': folder_path, 'depth': 1}, Language=language)
+ 
 
 def t_out(self):
     speed = self.data['speed']
