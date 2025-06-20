3ba1c215f58565909945166a5ab8bb1c9c647b52
Espen Moe-Nilssen <espen@medialog.no>
2025-06-20 15:26:07 +0200

    update dates and fix /@@contentlisting for fragmetnt

--- src/medialog/dutchmantheme/theme/fragments/collection_slider.py
+++ src/medialog/dutchmantheme/theme/fragments/collection_slider.py
@@ -1,7 +1,18 @@
 def get_items(self):
-    linked = self.data['linked_folder'] or self.data['select_folder']
-    folder = self.context.portal_catalog(UID=linked)
-    return folder[0].getObject()
+    language = self.context.Language()
+    linked = self.data['linked_folder'] or self.data['select_folder'] or self.get_uid()
+    if linked:
+        folder = self.context.portal_catalog(UID=linked)
+        mappe =  folder[0].getObject()
+        folder_path = '/'.join(mappe.getPhysicalPath())
+        if mappe.portal_type != 'Collection':
+            return self.context.portal_catalog(path={'query': folder_path, 'depth': 1}, Language=language)
+        
+        query = mappe.query
+        query_dict = {q['i']: q['v'] for q in query if 'i' in q and 'v' in q}
+        return self.context.portal_catalog(**query_dict) 
+
+ 
 
 def editmode(self):
     form = self.request.form
