305b08fc84cace5a5b97131a97b799775680b006
Espen Moe-Nilssen <espen@medialog.no>
2025-06-25 10:58:18 +0200

    update Collection Slider with Modified sort on etc

--- src/medialog/dutchmantheme/theme/fragments/collection_slider.py
+++ src/medialog/dutchmantheme/theme/fragments/collection_slider.py
@@ -6,7 +6,10 @@ def get_items(self):
         mappe =  folder[0].getObject()
         folder_path = '/'.join(mappe.getPhysicalPath())
         if mappe.portal_type != 'Collection':
-            return self.context.portal_catalog(path={'query': folder_path, 'depth': 1}, Language=language)
+            sorton = 'modified'
+            if 'sort_on' in self.data.keys():
+                sorton = self.data['sort_on']
+            return self.context.portal_catalog(path={'query': folder_path, 'depth': 1}, sort_on=sorton, Language=language)
         
         query = mappe.query
         query_dict = {q['i']: q['v'] for q in query if 'i' in q and 'v' in q}
