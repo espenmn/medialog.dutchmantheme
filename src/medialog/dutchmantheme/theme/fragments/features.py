0355cd546b0c9f32e975cf66533409e9379e5d03
Espen Moe-Nilssen <espen@medialog.no>
2026-05-22 17:06:04 +0200

    updarte features fragment

--- src/medialog/dutchmantheme/theme/fragments/features.py
+++ src/medialog/dutchmantheme/theme/fragments/features.py
@@ -31,18 +31,32 @@ def iconset(self):
 
 def get_items(self):
     data = self.data
-    keyword = data['keyword']
-    # if isinstance(u"", str):
-    #     keyword.encode('ascii','ignore')
+    keyword = self.data['keyword'] 
+    language = self.context.Language
     sorton = 'modified'
     sort_order = 'descending'
     if 'sort_order' in data.keys():
         sort_order = str(data['sort_order'])
     if 'sort_on' in data.keys():
         sorton = data['sort_on']
-    language = self.context.Language
-    return self.context.portal_catalog(Subject=keyword, sort_on=sorton, sort_order=sort_order, Language=language)
-
+        
+    linked = self.data['linked_folder'] 
+    if linked:
+        folder = self.context.portal_catalog(UID=linked)
+        mappe =  folder[0].getObject()
+        folder_path = '/'.join(mappe.getPhysicalPath())
+        if mappe.portal_type != 'Collection':
+            return self.context.portal_catalog(path={'query': folder_path, 'depth': 1}, sort_on=sorton, sort_order=sort_order, Language=language)
+        
+        query = mappe.query
+        query_dict = {q['i']: q['v'] for q in query if 'i' in q and 'v' in q}
+        return self.context.portal_catalog(**query_dict) 
+    
+    if keyword:
+        return self.context.portal_catalog(Subject=keyword, sort_on=sorton, sort_order=sort_order, Language=language)
+    
+    return None
+    
 def editmode(self):
     form = self.request.form
     if  '_layouteditor' in form:
